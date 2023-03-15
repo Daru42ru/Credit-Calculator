1d5b59c763b8be84c75514877df1fdcef9789864
Дарья Кротова <daru42ru@gmail.com>
2023-03-15 15:58:26 +0700

    src/CreditPaymentService.java

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,11 +1,7 @@
 public class CreditPaymentService {
-    public int calculate(double percent, int credit) {
+    public int calculate(int oneYear, double percent, int credit) {
         int result;
-        int i;
-        for (i = 12; i > 36; i = i + 12) {
-            System.out.println(i);
-        }
-        result = (int) (credit * ((percent / i / 100) / (1 - Math.pow(1 + percent / i / 100, -i))));
+        result = (int) (credit * ((percent / oneYear / 100) / (1 - Math.pow(1 + percent / oneYear / 100, -oneYear))));
         return result;
     }
 }
