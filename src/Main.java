c564223bdd15a4a3baaec6bfe66f6365e3cc2d62
Дарья Кротова <daru42ru@gmail.com>
2023-03-15 16:01:30 +0700

    src/Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,12 @@
 public class Main {
     public static void main(String[] args) {
         CreditPaymentService service = new CreditPaymentService();
+        int oneYear = 12;
         double percent = 9.99;
         int credit = 1_000_000;
-        int paymentOne = service.calculate(percent, credit);
+        int paymentOne = service.calculate(oneYear, percent, credit);
         System.out.println(paymentOne);
-        int paymentTwo = service.calculate(percent, credit);
-        System.out.println(paymentTwo);
-        int paymentThree = service.calculate(percent, credit);
-        System.out.println(paymentThree);
+        System.out.println(service.calculate(24, percent, credit));
+        System.out.println(service.calculate(36, percent, credit));
     }
 }
